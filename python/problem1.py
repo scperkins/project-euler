f9541dcbc236d4aaabd26231a49911e65b72a58e
Sean Perkins <perkinsean@gmail.com>
2015-02-26 19:57:47 -0800

    moved folders around and added haskell

---
+++ python/problem1.py
@@ -0,0 +1,21 @@
+'''If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+Find the sum of all the multiples of 3 or 5 below 1000.'''
+
+def dumb_way():
+    multiples = []
+    for number in xrange(1,1000):
+        if number % 3 == 0 or number % 5 == 0:
+            multiples.append(number)
+        else:
+            pass
+
+    print sum(multiples)
+'''
+def mults(x):
+    print(sum([mults(x-1) if x % 3 == 0 or x % 5 == 0]))
+'''
+
+if __name__ == "__main__":
+    dumb_way()
+   # mults(1000)
+    
